33908dc015fc9c79129e9c911a4b948fc4b4b9e3
Taylor Hornby <havoc@defuse.ca>
2014-02-08 16:17:51 -0700

    Fix copyright year.

--- Crypto.php
+++ Crypto.php
@@ -2,7 +2,7 @@
 
 /*
  * PHP Encryption Library
- * Copyright (c) 2013, Taylor Hornby
+ * Copyright (c) 2014, Taylor Hornby
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
