7560a7680d4c2da84f67240476bce8665eff966d
Scott Arciszewski <scott@arciszewski.me>
2015-05-05 15:33:43 -0400

    Update example.php

--- example.php
+++ example.php
@@ -4,33 +4,33 @@ require_once('Crypto.php');
       $key = \Defuse\Crypto\Crypto::CreateNewRandomKey();
       // WARNING: Do NOT encode $key with bin2hex() or base64_encode(),
       // they may leak the key to the attacker through side channels.
-  } catch (CryptoTestFailedException $ex) {
+  } catch (\Defuse\Crypto\CryptoTestFailedException $ex) {
       die('Cannot safely create a key');
-  } catch (CannotPerformOperationException $ex) {
+  } catch (\Defuse\Crypto\CannotPerformOperationException $ex) {
       die('Cannot safely create a key');
   }
 
   $message = "ATTACK AT DAWN";
   try {
       $ciphertext = \Defuse\Crypto\Crypto::Encrypt($message, $key);
-  } catch (CryptoTestFailedException $ex) {
+  } catch (\Defuse\Crypto\CryptoTestFailedException $ex) {
       die('Cannot safely perform encryption');
-  } catch (CannotPerformOperationException $ex) {
+  } catch (\Defuse\Crypto\CannotPerformOperationException $ex) {
       die('Cannot safely perform decryption');
   }
 
   try {
       $decrypted = \Defuse\Crypto\Crypto::Decrypt($ciphertext, $key);
-  } catch (InvalidCiphertextException $ex) { // VERY IMPORTANT
+  } catch (\Defuse\Crypto\InvalidCiphertextException $ex) { // VERY IMPORTANT
       // Either:
       //   1. The ciphertext was modified by the attacker,
       //   2. The key is wrong, or
       //   3. $ciphertext is not a valid ciphertext or was corrupted.
       // Assume the worst.
       die('DANGER! DANGER! The ciphertext has been tampered with!');
-  } catch (CryptoTestFailedException $ex) {
+  } catch (\Defuse\Crypto\CryptoTestFailedException $ex) {
       die('Cannot safely perform encryption');
-  } catch (CannotPerformOperationException $ex) {
+  } catch (\Defuse\Crypto\CannotPerformOperationException $ex) {
       die('Cannot safely perform decryption');
   }
 ?>
