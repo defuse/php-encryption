0345d601115d30847a7452e32609666a70780135
Scott <scott@paragonie.com>
2015-10-16 23:40:28 -0400

    Hard-coded list of acceptable classes to load.

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  */
 \spl_autoload_register(function ($class) {
     // Project-specific namespace prefix
-    $prefix = 'Defuse\\Crypto';
+    $prefix = 'Defuse\\Crypto\\';
 
     // Base directory for the namespace prefix
     $base_dir = __DIR__.'/src/';
@@ -22,37 +22,31 @@
     /**
      * unserialize() -> autoloader -> LFI hardening
      */
-    // Since we're on PHP 5.4+ we can use the [] syntax:
-    $whitelist = [
-        '\\Core',
-        '\\Crypto', 
-        '\\File',
-        '\\ExceptionHandler',
-        '\\StreamInterface',
-        '\\Exception\\CannotPerformOperationException',
-        '\\Exception\\CryptoException',
-        '\\Exception\\CryptoTestFailedException',
-        '\\Exception\\InvalidCiphertextException',
-    ];
-    if (!\in_array($relative_class, $whitelist)) {
-        var_dump($relative_class);
-        // Skip it.
-        return;
-    }
-
-    // Replace the namespace prefix with the base directory, replace namespace
-    // separators with directory separators in the relative class name, append
-    // with .php
-    $file = $base_dir.
-        \str_replace(
-            ['\\', '_'],
-            '/',
-            $relative_class
-        ).'.php';
-
-    // If the file exists, require it
-    if (\file_exists($file)) {
-        require $file;
+    $classmap = array(
+        'Core' =>
+            'Core.php',
+        'Crypto' =>
+            'Crypto.php', 
+        'File' =>
+            'File.php',
+        'ExceptionHandler' =>
+            'ExceptionHandler.php',
+        'StreamInterface' =>
+            'StreamInterface.php',
+        // Exceptions:
+        'Exception\\CannotPerformOperationException' =>
+            'Exception/CannotPerformOperationException.php',
+        'Exception\\CryptoException' =>
+            'Exception/CryptoException.php',
+        'Exception\\CryptoTestFailedException' =>
+            'Exception/CryptoTestFailedException.php',
+        'Exception\\InvalidCiphertextException' =>
+            'Exception/InvalidCiphertextException.php',
+    );
+    foreach ($classmap as $classname => $file) {
+        if ($classname === $relative_class) {
+            require $base_dir.$file;
+        }
     }
 });
 
