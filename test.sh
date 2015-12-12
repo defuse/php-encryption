2aee3b8ec74c943c1306a0832935bb048165229f
Taylor Hornby <havoc@defuse.ca>
2015-12-12 13:33:48 -0700

    Make test.sh only generate the big test plaintext if it's not already there.

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
-echo "Please wait while I create a large random test plaintext file..."
-dd if=/dev/urandom of=./test/unit/File/big-generated-file bs=1M count=200
-echo "Now running the tests..."
+
+BIG_GENERATED_FILE=./test/unit/File/big-generated-file
+if [ ! -e $BIG_GENERATED_FILE ] || [ $(wc -c < $BIG_GENERATED_FILE) -ne "209715200" ]; then
+    echo "Please wait while I create a large random test plaintext file..."
+    dd if=/dev/urandom "of=$BIG_GENERATED_FILE" bs=1M count=200
+fi
+
 ./test/phpunit.sh
 echo ""
