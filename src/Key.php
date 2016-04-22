793b50c516f539318acb144f1ec8aae3882ecffd
Taylor Hornby <taylor@defuse.ca>
2016-04-21 21:19:10 -0600

    I am an idiot.

--- src/Key.php
+++ src/Key.php
@@ -35,7 +35,7 @@ final class Key
      */
     public static function loadFromAsciiSafeString($saved_key_string)
     {
-        $key_bytes = Encoding::loadBytesFromChecksummedAsciiSafeString(self::KEY_CURRENT_VERSION, $savedKeyString);
+        $key_bytes = Encoding::loadBytesFromChecksummedAsciiSafeString(self::KEY_CURRENT_VERSION, $saved_key_string);
         return new Key($key_bytes);
     }
 
