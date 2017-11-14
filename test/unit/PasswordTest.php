13b94db47cd9b461128501894e342821804bf7b7
Rich Lott <forums@artfulrobot.uk>
2017-11-14 15:11:16 +0000

    Add changePassword method, addressing #371

--- test/unit/PasswordTest.php
+++ test/unit/PasswordTest.php
@@ -23,4 +23,23 @@ class PasswordTest extends PHPUnit_Framework_TestCase
         $pkey = KeyProtectedByPassword::createRandomPasswordProtectedKey('rightpassword');
         $key1 = $pkey->unlockKey('wrongpassword');
     }
+    /**
+     * Check that a new password was set.
+     */
+    public function testChangePassword()
+    {
+        $pkey1 = KeyProtectedByPassword::createRandomPasswordProtectedKey('password');
+        $pkey1_enc_ascii = $pkey1->saveToAsciiSafeString();
+        $key1 = $pkey1->unlockKey('password')->saveToAsciiSafeString();
+
+        $pkey1->changePassword('password', 'new password');
+        $pkey1_enc_ascii_new = $pkey1->saveToAsciiSafeString();
+        $key1_new = $pkey1->unlockKey('new password')->saveToAsciiSafeString();
+
+        // The encrypted_key should not be the same.
+        $this->assertNotSame($pkey1_enc_ascii, $pkey1_enc_ascii_new);
+
+        // The encrypted_key should not be the same.
+        $this->assertSame($key1, $key1_new);
+    }
 }
